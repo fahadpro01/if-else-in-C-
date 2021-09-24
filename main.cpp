9c40d9ae37970827e148c6a76dd3e18bd97ddbe3
Mohammed Fahad F S <52351395+fahadpro01@users.noreply.github.com>
2021-09-24 13:50:25 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char option;
+  cout<<"Select a option ";
+  cin>>option;
+
+if(option=='a')
+cout<<"Hello dude";
+else if (option=='b')
+cout<<"Hey dude";
+else if (option=='c')
+cout<<"Hai dude";
+else 
+cout<<"Iam still learing";
+
+}
